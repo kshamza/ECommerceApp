234366c2909b1dcd846e62b8962b89979fbc10ab
Kamal Hamza <115679756+kshamza@users.noreply.github.com>
2022-12-19 16:36:27 -0500

    cleaned up the cucumber options

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,14 +6,15 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features = "src/main/resources/features", 
-                 tags = "@smoke-test", 
-                 glue={"stepDefinitions"},
-                 plugin = {"pretty","json:target/cucumber-reports/reports.json",
+@CucumberOptions(features = "src/main/resources/features"
+        ,tags = "@smoke-test"
+        ,glue = {"stepDefinitions"}
+        ,plugin = {"pretty","json:target/cucumber-reports/reports.json",
                            "junit:target/cucumber-reports/Cucumber.xml",
-                           "html:target/cucumber-reports/reports2.html"},
-                 monochrome = true,
-                 dryRun = false)
+                           "html:target/cucumber-reports/reports2.html"}
+        ,monochrome = true
+        ,dryRun = false
+)
 
 public class TestRunner {
         
