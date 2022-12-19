22aa87b911dd83a688e96fa51cab7246c0d4e392
Kamal Hamza <115679756+kshamza@users.noreply.github.com>
2022-12-19 17:23:28 -0500

    added a slf4j logger

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,5 +1,9 @@
 package stepDefinitions;
 
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
@@ -8,6 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Hooks {
 
     private static WebDriver driver;
+    private Logger logger;
 
     public static WebDriver getDriver(){
         return driver;
@@ -15,6 +20,10 @@ public class Hooks {
 
     @Before
     public void user_opens_browser_and_navigates_to_test_site() throws InterruptedException{
+        
+        logger = LoggerFactory.getLogger(Hooks.class);
+        BasicConfigurator.configure();
+        
         String chromePath = System.getProperty("user.dir") + "/src/main/resources/browsers/chrome/chromedriver_108.exe";
         System.setProperty("webdriver.chrome.driver", chromePath);
         driver = new ChromeDriver();
