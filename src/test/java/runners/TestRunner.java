2262ab78890444b24d57331d9821f3e2c678091d
Kamal Hamza <115679756+kshamza@users.noreply.github.com>
2022-12-19 15:02:40 -0500

    added cucumber features and tags

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -5,11 +5,16 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/main/resources/features"
-//        , tags = "@current-feature"
-        , glue={"stepDefinitions"}
-)
+
+@CucumberOptions(features = "src/main/resources/features", 
+                 tags = "@smoke-test", 
+                 glue={"stepDefinitions"},
+                 plugin = {"pretty","json:target/cucumber-reports/reports.json",
+                           "junit:target/cucumber-reports/Cucumber.xml",
+                           "html:target/cucumber-reports/reports2.html"},
+                 monochrome = true,
+                 dryRun = false)
 
 public class TestRunner {
+        
 }
